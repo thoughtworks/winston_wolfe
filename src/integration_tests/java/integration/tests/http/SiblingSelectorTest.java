ab2c852d0a6b9fce5396db5be6db3553be70032f
Ryan Boucher <ryan.boucher@distributedlife.com>
2013-04-10 13:55:20 +1000

    in progress - sibling validation

---
+++ src/integration_tests/java/integration/tests/http/SiblingSelectorTest.java
@@ -0,0 +1,44 @@
+package integration.tests.http;
+
+import com.thoughtworks.winstonwolfe.application.WinstonWolfe;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import infrastructure.MockSystemUnderTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+@Ignore
+public class SiblingSelectorTest {
+    MockSystemUnderTest mockSUT;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Before
+    public void spinUpServer() throws Exception {
+        mockSUT = new MockSystemUnderTest(getResourceFileContents("xml/out_with_groups.xml"));
+        mockSUT.startServer();
+    }
+
+    @After
+    public void stopServer() throws Exception {
+        mockSUT.stopServer();
+    }
+
+    @Test
+    public void selectorMatchPasses() throws Exception {
+        URL config = ClassLoader.getSystemResource("yaml/http_config.yaml");
+        URL script = ClassLoader.getSystemResource("yaml/siblingValidation/test.yaml");
+
+        WinstonWolfe.main(new String[]{config.getPath(), script.getPath()});
+    }
+
+    private String getResourceFileContents(String filename) throws IOException {
+        URL url = ClassLoader.getSystemResource(filename);
+
+        return new Scanner(new File(url.getPath())).useDelimiter("\\Z").next();
+    }
+}
